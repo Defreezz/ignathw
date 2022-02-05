5a750d9db8447238ed8707d3170d5e418b786cd0
Artsiom <yatsevich-artsiom@yandex.by>
2022-02-05 17:40:53 +0300

    added hw13 without redux

--- src/p2-homeworks/h13/HW13.tsx
+++ src/p2-homeworks/h13/HW13.tsx
@@ -5,6 +5,7 @@ import s from "../h12/HW12.module.css";
 import {useSelector} from "react-redux";
 import {AppStoreType} from "../h10/bll/store";
 import {CircularProgress} from "@material-ui/core";
+import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 
 export const HW13 = () => {
     const [response, setResponse] = useState<CommonResponse>({errorText: '', info: ''})
@@ -12,22 +13,22 @@ export const HW13 = () => {
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const {selectTheme} = useSelector((state: AppStoreType) => state.theme)
 
-    useEffect(() => {
-        (async function () {
+
+        async function sendResponse () {
             try {
                 setIsLoading(true)
                 const res = await hwAPI.sendStatusCheckbox(checked)
                 setResponse(res)
                 setIsLoading(false)
-            } catch (e: any) {//хз как типизировать e
+            }
+            catch (e: any) {//хз как типизировать e
                 const error = e.response.data as Error
                 if (error) {
                     setIsLoading(false)
                     setResponse(error)
                 } else console.log('error:', e)
             }
-        }())
-    }, [checked])
+        }
 
     const {errorText, info} = response
     return (
@@ -38,19 +39,22 @@ export const HW13 = () => {
                 homeworks 13
             </span>
             <br/>
-            <SuperCheckbox
-                onChangeChecked={setChecked}
+            <div style={{marginTop:"15px"}}><SuperCheckbox
                 checked={checked}
+                onChangeChecked={setChecked}
             />
-            <br/>
-            {isLoading
-                ? <CircularProgress color="secondary"/>
-                : <div className={s[selectTheme + '-text']}>
-                    {errorText}
-                    <br/>
-                    {info}
-                </div>
-            }
+                <SuperButton onClick={sendResponse}>
+                    Send checkbox Status
+                </SuperButton>
+                <br/>
+                {isLoading
+                    ? <CircularProgress color="secondary"/>
+                    : <div className={s[selectTheme + '-text']}>
+                        {errorText}
+                        <br/>
+                        {info}
+                    </div>
+                }</div>
         </div>
     )
 }
--- src/p2-homeworks/h4/common/c2-SuperButton/SuperButton.tsx
+++ src/p2-homeworks/h4/common/c2-SuperButton/SuperButton.tsx
@@ -8,12 +8,11 @@ type SuperButtonPropsType = DefaultButtonPropsType & {
     red?: boolean
 }
 
-const SuperButton: React.FC<SuperButtonPropsType> = (
-    {
-        red,
-        className,
-        ...restProps// все остальные пропсы попадут в объект restProps, там же будет children
-    }
+const SuperButton: React.FC<SuperButtonPropsType> = ({
+                                                         red,
+                                                         className,
+                                                         ...restProps// все остальные пропсы попадут в объект restProps, там же будет children
+                                                     }
 ) => {
     const finalClassName = `${red ? s.red : s.default} ${className}`
 
